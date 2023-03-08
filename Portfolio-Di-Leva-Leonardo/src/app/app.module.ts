4aa7d8e1f4a3d1b60633727f966d5b21f3789bd1
Leo-Di-Leva <leo.dileva@hotmail.com>
2023-03-07 22:24:29 -0300

    Punto de Respaldo 02 // Backup save 02
    Progresando con la experiencia/educacion del contenido del portfolio.

--- Portfolio-Di-Leva-Leonardo/src/app/app.module.ts
+++ Portfolio-Di-Leva-Leonardo/src/app/app.module.ts
@@ -8,6 +8,10 @@ import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { SocialComponent } from './components/social/social.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
 
 @NgModule({
   declarations: [
@@ -16,11 +20,15 @@ import { SocialComponent } from './components/social/social.component';
     LogoAPComponent,
     AcercaDeComponent,
     BannerComponent,
-    SocialComponent
+    SocialComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
