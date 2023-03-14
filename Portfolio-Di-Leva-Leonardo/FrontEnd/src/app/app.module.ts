f4d34c94a50f8f8088590090d04fae606f925dbb
Leo-Di-Leva <leo.dileva@hotmail.com>
2023-03-14 10:36:54 -0300

    Division en Carpetas (FrontEnd)

--- Portfolio-Di-Leva-Leonardo/FrontEnd/src/app/app.module.ts
+++ Portfolio-Di-Leva-Leonardo/FrontEnd/src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { BannerComponent } from './components/banner/banner.component';
-import { SocialComponent } from './components/social/social.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
@@ -23,7 +21,6 @@ import { FooterComponent } from './components/footer/footer.component';
     LogoAPComponent,
     AcercaDeComponent,
     BannerComponent,
-    SocialComponent,
     ExperienciaComponent,
     EducacionComponent,
     HardComponent,
