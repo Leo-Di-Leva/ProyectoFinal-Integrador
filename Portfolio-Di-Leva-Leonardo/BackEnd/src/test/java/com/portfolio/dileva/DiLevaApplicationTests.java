32115b03b6b230a620e425651cab07b1c1b46677
Leo-Di-Leva <leo.dileva@hotmail.com>
2023-03-14 15:24:00 -0300

    Backup Point

---
+++ Portfolio-Di-Leva-Leonardo/BackEnd/src/test/java/com/portfolio/dileva/DiLevaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.dileva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiLevaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
