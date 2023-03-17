b9d4fc1a85030ed8004346decffb437e1a77ce71
Leo-Di-Leva <leo.dileva@hotmail.com>
2023-03-17 07:33:25 -0300

    Primera Parte del Backend

---
+++ Portfolio-Di-Leva-Leonardo/BackEnd/src/main/java/com/portfolio/dileva/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfolio.dileva.Repository;
+
+import com.portfolio.dileva.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long>{
+    
+}
